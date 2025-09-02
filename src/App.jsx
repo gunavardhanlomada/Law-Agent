b1f8fdc0e783f8a6d2a81840b952095bbf7a841f
gunavardhan26 <gunavardhan2665@gmail.com>
2025-09-02 19:26:02 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>Home Page</div>} />
+      <Route path="/legalsearch" element={<div>legalsearch Page</div>} />
+      <Route path="/aichat" element={<div>ai chat Page</div>} />
+      <Route path="/researchagent" element={<div>research agent Page</div>} />
+      <Route path="/resources" element={<div>resources Page</div>} />
+      <Route path="*" element={<div>404 Not Found</div>} />
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
