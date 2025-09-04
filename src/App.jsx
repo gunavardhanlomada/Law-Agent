dd03325e3c252f22d82de2ae820f807a8eccef32
koushikmadisetty <saikoushik510@gmail.com>
2025-09-04 13:11:34 +0530

    Resources Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 // import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
+import LegalResources from './Components/resources';
+
 
 function App() {
 
@@ -14,7 +16,8 @@ function App() {
       <Route path="/legalsearch" element={<div>legalsearch Page</div>} />
       <Route path="/aichat" element={<div>ai chat Page</div>} />
       <Route path="/researchagent" element={<div>research agent Page</div>} />
-      <Route path="/resources" element={<div>resources Page</div>} />
+      <Route path="/resources" element={<LegalResources />} />
+      
       <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
     </BrowserRouter>
