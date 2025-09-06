f8145cc1cbfce6f5ce186544d59c44a1a9d76b37
gunavardhan26 <gunavardhan2665@gmail.com>
2025-09-06 21:30:32 +0530

    Reseach Agent Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 // import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
+import ResearchPaperAgent from './Components/ResearchPaperAgent';
 
 function App() {
 
   return (
     <>
-    <NavBar/>
+    
     <BrowserRouter>
+    <NavBar/>
     <Routes>
       <Route path="/" element={<div>Home Page</div>} />
       <Route path="/legalsearch" element={<div>legalsearch Page</div>} />
       <Route path="/aichat" element={<div>ai chat Page</div>} />
-      <Route path="/researchagent" element={<div>research agent Page</div>} />
+      <Route path="/researchagent" element={<ResearchPaperAgent/>} />
       <Route path="/resources" element={<div>resources Page</div>} />
       <Route path="*" element={<div>404 Not Found</div>} />
     </Routes>
